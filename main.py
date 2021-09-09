719f32005b388a99b018c034c9c57b9d00794fd9
TheTNTBlower77 <xanderandrewritua@gmail.com>
2021-09-09 12:11:07 +0800

    new

--- main.py
+++ main.py
@@ -7,5 +7,16 @@ client = discord.Client()
 async def on_ready():
     print(f"Logged in as {client.user}.")
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("//"):
+        messageCut = message[2:]
+
+        if messageCut == "hello":
+            await message.channel.send("*Bark!*")
+
 botToken = os.environ['TOKEN']
 client.run(botToken)
