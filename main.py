f46233113f4f0571277ccb71ed7fed6c391d389d
TheTNTBlower77 <xanderandrewritua@gmail.com>
2021-09-10 17:53:10 +0800

    new

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import discord
 import discord.ext.commands
 import os
+import asyncio
 
+commandPrefix = "//"
 bot = discord.Client()
-bot = discord.ext.commands.Bot(command_prefix='!')
+bot = discord.ext.commands.Bot(command_prefix=commandPrefix)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}.")
+
 
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"*Bark! I'm an actual bot! :D*")
 
 @bot.command()
-async def claimchannel(ctx, arg):
-    await ctx.channel.edit(topic=arg)
-    await ctx.send(f"hi just wanna let ya know it works")
-    
+async def claimchannel(ctx, place):
+    currentTopic = ctx.channel.topic
+    currentTopicSplit = currentTopic.split(" | ")
+    newTopicList = [currentTopicSplit[0], " | ", f"Channel claimed. Current location: {place}"]
 
-botToken = os.environ['TOKEN']
+    await ctx.channel.edit(topic="".join(newTopicList))
+    await ctx.send(f"Channel claimed.")
+
+
+@bot.command()
+async def killswitch(ctx):
+    await ctx.send("O- *baiii-*")
+    await bot.logout()
+
+botToken = os.environ['CANITOKEN']
 bot.run(botToken)
