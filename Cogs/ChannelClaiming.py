aea5736849959a0030aa902859f7d32a5da9ab7c
TheTNTBlower77 <xanderandrewritua@gmail.com>
2021-10-03 21:11:32 +0800

    switch to new wrapper

--- Cogs/ChannelClaiming.py
+++ Cogs/ChannelClaiming.py
@@ -5,6 +5,7 @@ import main
 from Functions import ExtraFunctions as ef
 from Functions import FirebaseInteraction as fi
 from Functions import CustomExceptions as ce
+from Functions import CommandWrapper as cw
 
 
 class ChannelClaim(cmds.Cog):
@@ -14,7 +15,6 @@ class ChannelClaim(cmds.Cog):
 
     async def path(self, ctx: cmds.Context):
         return ["guilds", ctx.guild.id, "claimChannelData"]
-
 
     async def getChannels(self, ctx):
         path = await self.path(ctx)
@@ -74,9 +74,18 @@ class ChannelClaim(cmds.Cog):
         await embedMessage.edit(embed=embed)
 
 
-    @cmds.command(aliases=["cc"])
-    @cmds.guild_only()
-    @cmds.cooldown(1, 60 * 2, cmds.BucketType.user)
+    @cw.command(
+        category=cw.Categories.channelClaiming,
+        description="Claims / unclaims the current RP channel to a specific location.",
+        parameters={
+            "[claim | unclaim]": "Tells if you want to claim or unclaim the current RP channel.",
+            "location": "The location of where you want the channel to be in. Surround the location with quotes (example: `\"Imagination Room\"`).\nNote that __this parameter doesn't have to be filled in when you're `unclaim`ing__ the channel."
+        }, aliases=["cc"],
+        cooldown=60 * 2, cooldownType="user",
+        exampleUsage=[
+            f"{main.commandPrefix}claimchannel claim \"Quaz's HQ\"",
+            f"{main.commandPrefix}claimchannel unclaim"
+        ])
     async def claimchannel(self, ctx, type, place=None, *dump):
         if not await self.isRpChannel(ctx):
             await ef.sendError(ctx, f"*This isn't an RP channel! >:(*", resetCooldown=True)
@@ -113,9 +122,14 @@ class ChannelClaim(cmds.Cog):
         await self.updateEmbed(ctx)
 
 
-    @cmds.command(aliases=["cce"])
-    @cmds.guild_only()
-    @cmds.has_role(main.adminRole)
+    @cw.command(
+        category=cw.Categories.channelClaiming,
+        description="Adds / removes the channel as an RP channel.",
+        parameters={
+            "[add | remove]": "Tells if you want to add or remove a channel as an RP channel.",
+            "channel": "Channel that you want to add / remove as an RP channel."
+        }, aliases=["cce"],
+        requireAdmin=True)
     async def claimchanneledit(self, ctx, type, channelMention, *dump):
         path = await self.path(ctx)
         claimChannels = await self.getChannels(ctx)
@@ -167,10 +181,13 @@ class ChannelClaim(cmds.Cog):
         
         await self.updateEmbed(ctx)
 
-
-    @cmds.command(aliases=["ccm"])
-    @cmds.guild_only()
-    @cmds.has_role(main.adminRole)
+    @cw.command(
+        category=cw.Categories.channelClaiming,
+        description="Changes where the embed for displaying claimed channels are sent.",
+        parameters={
+            "channel": "Channel where the embed will be put in."
+        }, aliases=["ccm"],
+        requireAdmin=True)
     async def claimchannelembed(self, ctx, channelMention, *dump):
         path = await self.path(ctx)
         try:
@@ -191,10 +208,11 @@ class ChannelClaim(cmds.Cog):
         await ctx.send(f"*Changed claim display channel to {channelMention}! :D*")
     
 
-
-    @cmds.command(aliases=["ccu"])
-    @cmds.guild_only()
-    @cmds.has_role(main.adminRole)
+    @cw.command(
+        category=cw.Categories.channelClaiming,
+        description="Updates the embed for displaying claimed channels.",
+        aliases=["ccm"],
+        requireAdmin=True)
     async def claimchannelupdate(self, ctx):
         await ctx.send(f"*Updating embed...*")
         await self.updateEmbed(ctx)
