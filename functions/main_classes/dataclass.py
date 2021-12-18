0697276bc6a75108c166fc54d81d20e3b876f2c3
TNTzx <xanderandrewritua@gmail.com>
2021-12-18 16:19:31 +0800

    new dataclass

--- functions/main_classes/dataclass.py
+++ functions/main_classes/dataclass.py
@@ -6,7 +6,9 @@
 # pylint: disable=too-many-instance-attributes
 # pylint: disable=too-many-public-methods
 
+from __future__ import annotations
 import abc
+
 import functions.other_functions as o_f
 
 
@@ -39,17 +41,43 @@ class DataclassSub(Dataclass):
     """Base class for dataclasses inside dataclasses."""
 
 
-class StandardDataclass(abc.ABC, Dataclass):
+def init_wrapper(init, end_init):
+    """Wraps the init function of DataclassConvention classes."""
+    def wrapper(*args, **kwargs):
+        init(*args, **kwargs)
+        end_init(*args, **kwargs)
+    return wrapper
+
+class DataclassConvention(Dataclass):
+    """Base class for standard and non-standard dataclasses."""
+
+    @abc.abstractmethod
+    def end_init(self, data=None):
+        """Called at the end of the __init__ function to implement the data."""
+
+    def __init_subclass__(cls) -> None:
+        cls.__init__ = init_wrapper(cls.__init__, cls.end_init)
+
+
+class StandardDataclass(abc.ABC, DataclassConvention):
     """Base class for standard dataclasses.
     These are classes used as a base for other non-standard dataclasses."""
 
     def default_data(self):
+        """Returns the default data."""
         return self.get_dict()
-    
-    def end_init(self):
-        self.from_dict(self.default_data())
 
-class NonStandardDataclass(abc.ABC, Dataclass):
+    def end_init(self, data=None):
+        if data is None:
+            self.from_dict(self.default_data())
+            return
+        self.from_dict(self.dict_from_nonstandard(data))
+
+    @abc.abstractmethod
+    def dict_from_nonstandard(self, data):
+        """Creates a dictionary based off of the nonstandard dataclass."""
+
+class NonStandardDataclass(abc.ABC, DataclassConvention):
     """Base class for non-standard dataclasses.
     These are classes that are slight derivations from a standard dataclass."""
 
@@ -57,18 +85,11 @@ class NonStandardDataclass(abc.ABC, Dataclass):
     def default_class(self):
         """Default class to bind to."""
 
-    def __init__(self, data: StandardDataclass | dict = None) -> None:
-        super().__init__()
-
     def get_default_dict(self):
         """Gets the default dictionary for this dataclass."""
         return self.__class__(self.default_class()).get_dict()
 
-    def dict_from_default(self, data: StandardDataclass):
-        """Returns a dictionary with default values of the default dataclass converted into this dataclass."""
-        return {}
-
-    def end_init(self, data: StandardDataclass | dict = None):
+    def end_init(self, data=None):
         """Added to the end of the __init__ function to put the data into the dataclass's fields."""
         if isinstance(data, self.default_class):
             converted_data = self.dict_from_default(data)
@@ -78,3 +99,8 @@ class NonStandardDataclass(abc.ABC, Dataclass):
             converted_data = self.dict_from_default(self.default_class())
 
         self.from_dict(converted_data)
+
+    @abc.abstractmethod
+    def dict_from_default(self, data: StandardDataclass):
+        """Returns a dictionary with default values of the default dataclass converted into this dataclass."""
+        return {}
