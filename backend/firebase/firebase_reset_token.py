8135dabae72cd5d4f768143a4df3018dc7bc551d
TNTzx <xanderandrewritua@gmail.com>
2022-01-24 17:02:47 +0800

    reset token

--- backend/firebase/firebase_reset_token.py
+++ backend/firebase/firebase_reset_token.py
@@ -5,17 +5,18 @@ import datetime
 import pytz
 
 import global_vars.variables as vrs
+import global_vars.loggers as lgr
 
 
 def start_loop():
     """Starts the loop to reset the token periodically."""
     while True:
-        time.sleep(60 * 30)
+        time.sleep(1)
         vrs.fb_user = vrs.fb_auth.refresh(vrs.fb_user['refreshToken'])
 
-        timezone = pytz.timezone('Asia/Manila')
-        time_obj = datetime.datetime.now(timezone)
-        time_str = time_obj.strftime("%I:%M:%S %p | %a, %d/%m/%Y")
+        # timezone = pytz.timezone('Asia/Manila')
+        # time_obj = datetime.datetime.now(timezone)
+        # time_str = time_obj.strftime("%I:%M:%S %p | %a, %d/%m/%Y")
 
-        print(f"Token refreshed at {time_str}.")
+        lgr.log_firebase.info("Token reset.")
         
