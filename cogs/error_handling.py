7e3459755298669d62b60eb7f5e37975c063f749
TNTzx <xanderandrewritua@gmail.com>
2022-01-24 16:04:54 +0800

    logging set up

--- cogs/error_handling.py
+++ cogs/error_handling.py
@@ -5,12 +5,16 @@
 # pylint: disable=unused-argument
 # pylint: disable=no-self-use
 
-# import nextcord as nx
+import asyncio
+import traceback as tr
+import nextcord as nx
 import nextcord.ext.commands as cmds
 
 import global_vars.variables as vrs
-import backend.other_functions as o_f
+import global_vars.loggers as lgr
+import backend.exceptions.custom_exc as c_e
 import backend.exceptions.send_error as s_e
+import backend.other_functions as o_f
 
 
 CMD_PREFIX = vrs.CMD_PREFIX
@@ -26,7 +30,7 @@ class ErrorHandler(cmds.Cog):
 
         if checkexc(cmds.CommandOnCooldown):
             time = o_f.format_time(int(str(round(exc.retry_after, 0))[:-2]))
-            await s_e.send_error(ctx, f"The command is on cooldown for `{time}` more!", cooldown_reset=True)
+            await s_e.send_error(ctx, f"The command is on cooldown for `{time}` more!")
             return
 
         if checkexc(cmds.MissingRole):
@@ -50,18 +54,29 @@ class ErrorHandler(cmds.Cog):
             return
 
         if checkexc(cmds.CommandInvokeError):
-            if str(exc.__cause__) == "Exited Function.":
+            if isinstance(exc.original, c_e.ExitFunction):
                 return
+
             if hasattr(exc.original, "status"):
                 if exc.original.status == 403:
-                    print(f"Forbidden. Code {exc.original.code}: {exc.original.text}")
+                    error_message = f"Forbidden from sending. Code {exc.original.code}: {exc.original.text}"
+                    lgr.log_discord_forbidden.warning(error_message)
                     return
+
+            if isinstance(exc.original, asyncio.TimeoutError):
+                await s_e.send_error(ctx, "Command timed out. Please run the command again.")
+                return
+
+            if isinstance(exc.original, nx.NotFound):
+                error_message = f"Not found. Code {exc.original.code}: {exc.original.text}"
+                lgr.log_discord_forbidden.warning(error_message)
+                return
 
         if checkexc(cmds.CommandNotFound):
             return
 
+        lgr.log_global_exc.error("".join(tr.format_exception(exc.original)))
         await s_e.send_error(ctx, "Something went wrong. This error has been reported to the owner of the bot.", exc=exc, send_owner=True, send_console=True)
-
 
 
 def setup(bot):
